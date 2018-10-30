a145aaededa1481320851e844ba19b396c8b2ac1
bogdan-brindusa <44159540+bogdan-brindusa@users.noreply.github.com>
2018-10-30 20:30:21 +0000

    A simple 'Hello World' program

---
+++ my_first_program.py
@@ -0,0 +1,7 @@
+# Printing some text on the screen
+print("Hello world!")
+print("My name is Bogdan. I am a student in IT")
+
+# Asking user to give some information and then printing on the screen
+college = input("Where are you studying? ")
+print("You are studying at", college)
