db1944ec40c58de26314939abd51e5894000d012
bogdan-brindusa <44159540+bogdan-brindusa@users.noreply.github.com>
2018-11-03 13:51:00 +0000

    Update random_number_game.py

--- random_number_game.py
+++ random_number_game.py
@@ -1,9 +1,13 @@
+# A simple game using 'while' loop and a function from module 'random'
+
 import random
 
 number = random.randint(1, 100)
 
+# user is prompted to choose a number in a range
 print("Guess a magic number between 1 and 100")
 
+# 'while' loop will finish when user guess the number
 guess = -1
 while guess != number:
     guess = int(input("Enter your guess: "))
