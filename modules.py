06519b8634cf113872c840f67f859d038c06d4b4
bogdan-brindusa <44159540+bogdan-brindusa@users.noreply.github.com>
2018-11-03 14:36:47 +0000

    Update modules.py
    Using functions

--- modules.py
+++ modules.py
@@ -2,16 +2,24 @@
 
 import math
 
+# function that prints square root of a and b
+def square_root(x, y):
+  print('Square root of %d is: %.2f' %(x, math.sqrt(x)))
+  print('Square root of %d is: %.2f' %(y, math.sqrt(y)))
+
+# function that prints result of a to the power of b
+def power(x, y):
+  print('%d to the power of %d is: %d' %(x, y, math.pow(x, y)))
+
+# function that prints length of the vector from origin (0, 0) to point (a, b)
+def vector(x, y):
+  print('Vector length from origin(0, 0) to point (%d, %d) is: %.2f' %(x, y, math.hypot(x, y)))
+
 # working with two numeric variables
 a = int(input('Give a value for a: '))
 b = int(input('Give a value for b: '))
 
-# print square root of a and b
-print('Square root of %d is: %.2f' %(a, math.sqrt(a)))
-print('Square root of %d is: %.2f' %(b, math.sqrt(b)))
-
-# print result of a to the power of b
-print('%d to the power of %d is: %d' %(a, b, math.pow(a, b)))
-
-# print length of the vector from origin (0, 0) to point (a, b)
-print('Vector length from origin(0, 0) to point (%d, %d) is: %.2f' %(a, b, math.hypot(a, b)))
+# calling functions
+square_root(a, b)
+power(a, b)
+vector(a, b)
